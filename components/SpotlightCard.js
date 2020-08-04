b7a644489db170d97184fedb8a9df2d6348edca9
ccrum292 <ccrum292@gmail.com>
2020-08-03 20:55:41 -0400

    altered background and added full screen view for pictures

--- components/SpotlightCard.js
+++ components/SpotlightCard.js
@@ -2,6 +2,8 @@ import { useState } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faExpandAlt } from '@fortawesome/free-solid-svg-icons'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import ButtonOne from "./ButtonOne"
 import Link from 'next/link'
 
@@ -10,6 +12,7 @@ export default function SpotlightCard(props){
   const numberOfImages = props.img.length
   const [imgIndex, setImgIndex] = useState(0) 
   const [imgObj, setImgObj] = useState(props.img[0]);
+  const [expandedImgDiv, setExpandedImgDiv] = useState(false)
   
   const handleImgChangeRight = () => {
     console.log(`first imgIndex ${imgIndex}`)
@@ -43,14 +46,16 @@ export default function SpotlightCard(props){
         <div className="absolute bottom-0 my-auto w-full h-full flex items-center  justify-between">
           <FontAwesomeIcon onClick={e =>{
             e.preventDefault()
-            console.log("left")
             handleImgChangeLeft()
           }} className="cursor-pointer ml-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110" style={{ color: "#ecc94b" }} size="2x" icon={faChevronLeft} />
           <FontAwesomeIcon onClick={e =>{
             e.preventDefault()
-            console.log("right")
             handleImgChangeRight()
           }} className="cursor-pointer mr-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110" style={{ color: "#ecc94b" }} size="2x" icon={faChevronRight} />
+          <FontAwesomeIcon onClick={e =>{
+            e.preventDefault()
+            setExpandedImgDiv(()=>!expandedImgDiv)
+          }} className="absolute bottom-0 right-0 cursor-pointer m-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110" style={{ color: "#ecc94b" }} size="2x" icon={faExpandAlt} />
         </div>
       </div>
       <div className="flex flex-col mb-2">
@@ -61,7 +66,27 @@ export default function SpotlightCard(props){
       <video controls>
         <source src={props.videoSrc} type="video/mp4"></source>
       </video>
+      {expandedImgDiv ? 
+      <div className="fixed top-0 left-0 min-h-screen min-w-full z-50 bg-gray-800 bg-opacity-90 flex justify-center items-center">
+        <img className="object-contain w-full h-screen z-0" src={imgObj.src} alt={imgObj.alt}/>
+        <div className="absolute bottom-0 my-auto w-full h-full flex items-center  justify-between">
+          <FontAwesomeIcon onClick={e =>{
+            e.preventDefault()
+            handleImgChangeLeft()
+          }} className="cursor-pointer ml-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110" style={{ color: "#ecc94b" }} size="2x" icon={faChevronLeft} />
+          <FontAwesomeIcon onClick={e =>{
+            e.preventDefault()
+            handleImgChangeRight()
+          }} className="cursor-pointer mr-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110" style={{ color: "#ecc94b" }} size="2x" icon={faChevronRight} />
+          <FontAwesomeIcon onClick={e =>{
+            e.preventDefault()
+            setExpandedImgDiv(()=>!expandedImgDiv)
+          }} className="absolute top-0 right-0 cursor-pointer mr-4 mt-2 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110" style={{ color: "#ecc94b" }} size="2x" icon={faTimes} />
+        </div>
+      </div>: null
+      }
     </div>
   )
 }
 
+// lg:object-fill for img?
