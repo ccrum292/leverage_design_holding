44ddea522da0146d4cf1dcfe98b38e1cd89e8fb8
ccrum292 <ccrum292@gmail.com>
2020-08-17 21:01:31 -0400

    added privacy policy

--- pages/legal/index.js
+++ pages/legal/index.js
@@ -45,7 +45,19 @@ export default function IndexPage() {
         </div>
       </header>
       <main className="flex-grow">
-
+      <div className="lg:w-1/3 lg:mx-auto">
+          <div className="shadowClass grid grid-col-1 mx-2">
+            <div className="bg-yellow-300">
+              <h3 className="jost mx-2 mt-4 text-xl font-bold border-b-2 border-black">
+                Privacy Policy
+                </h3>
+              <p className="jost m-4">
+                Click the button below to view our privacy policy on termsfeed.com.
+                </p>
+                <button onClick={() => window.location.href = "https://www.termsfeed.com/live/a0629d10-2c63-4262-b89b-65c8b51f6b79"} className="text-xl text-center text-sm text-white bg-gray-700 shadowClass mx-4 mb-4 cursor-pointer p-1 self-center transition duration-700 ease-in-out transform duration-700 hover:text-black hover:font-black hover:bg-gray-500 hover:scale-105">View</button>
+            </div>
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
