32689ea8d32abcc6ffc142f2d85bd8b0aa76588a
Caleb Crum <ccrum292@pop-os.localdomain>
2020-08-29 13:48:25 -0400

    centered ButtonDownload on BigProductCard

--- components/BigProductCard.js
+++ components/BigProductCard.js
@@ -10,11 +10,11 @@ export default function BigProductCard(props) {
         <div className="mt-4 mx-2 lg:mx-2 lg:my-auto lg:w-1/2">
           <CarouselWithImgPreview img={props.img}/>
         </div>
-        <div className="lg:w-1/2">
-          <div className="flex flex-col mb-2">
+        <div className="lg:w-1/2 flex">
+          <div className="flex-grow flex flex-col mb-2">
             <h3 className="jost mx-2 mt-4 text-xl font-bold">{props.title}</h3>
             <p className="jost mx-2 mb-2">{props.text}</p>
-            <div className="flex flex-col justify-center">
+            <div className="flex-grow flex flex-col justify-center">
               <ButtonDownload route={props.firstButton.buttonRoute} fileName={props.firstButton.buttonFileName} text={props.firstButton.buttonText}/>
               <ButtonDownload route={props.secondButton.buttonRoute} fileName={props.secondButton.buttonFileName} text={props.secondButton.buttonText}/>
               <ButtonDownload route={props.thirdButton.buttonRoute} fileName={props.thirdButton.buttonFileName} text={props.thirdButton.buttonText}/>
