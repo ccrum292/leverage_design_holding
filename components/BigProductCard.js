afe5c305c101c55bcaf1530fe78529264e898bc4
Caleb Crum <ccrum292@pop-os.localdomain>
2020-08-29 13:39:34 -0400

    fixed button bug on products page, also altered margin on bigproductcard

--- components/BigProductCard.js
+++ components/BigProductCard.js
@@ -4,7 +4,7 @@ import CarouselWithImgPreview from "../components/CarouselWithImgPreview"
 
 export default function BigProductCard(props) {
   return(
-    <div className="shadowClass bg-yellow-300 my-4 mx-2">
+    <div className={`shadowClass bg-yellow-300 ${props.margin}`}>
       <iframe height="200" className="w-full sm:h-120 " src={props.videoSrc} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"></iframe>
       <div className="flex flex-col lg:flex-row">
         <div className="mt-4 mx-2 lg:mx-2 lg:my-auto lg:w-1/2">
@@ -14,7 +14,7 @@ export default function BigProductCard(props) {
           <div className="flex flex-col mb-2">
             <h3 className="jost mx-2 mt-4 text-xl font-bold">{props.title}</h3>
             <p className="jost mx-2 mb-2">{props.text}</p>
-            <div className="flex flex-col justify-center w-full">
+            <div className="flex flex-col justify-center">
               <ButtonDownload route={props.firstButton.buttonRoute} fileName={props.firstButton.buttonFileName} text={props.firstButton.buttonText}/>
               <ButtonDownload route={props.secondButton.buttonRoute} fileName={props.secondButton.buttonFileName} text={props.secondButton.buttonText}/>
               <ButtonDownload route={props.thirdButton.buttonRoute} fileName={props.thirdButton.buttonFileName} text={props.thirdButton.buttonText}/>
