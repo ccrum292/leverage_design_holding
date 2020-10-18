ce29df87a4646fc93c88a10494c4e017c6039093
ccrum292 <ccrum292@gmail.com>
2020-10-17 21:52:47 -0400

    updated ReadMe

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Leverage Design Holding Web Page
 
-Deployed: https://leveragedesignholding-git-master.ccrum292.vercel.app/
+## Deployment
+
+https://leveragedesignholding.com
+
+## Description
+
+The objective of this project was to create a fast, serverless, website for Leverage Design Holding, LLC.  Next.js and Vercel were used to accomplish the serverless functionality of this app.  Additional technologies used include: Tailwindcss, a lightweight css framework, and NameCheap, a domain name registrar.  For additional information, contact Caleb Crum at ccrum292@gmail.com for more information.        
+
+## Images
+
+![Leverage Design Holding Home Page](/public/ldh_home_lg.gif)
+
+![Leverage Design Holding Home Page](/public/ldh_products_lg.gif)
--- components/SpotlightCard.js
+++ components/SpotlightCard.js
@@ -1,89 +1,127 @@
-import { useState } from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
-import { faExpandAlt } from '@fortawesome/free-solid-svg-icons'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
-import ButtonOne from "./ButtonOne"
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faExpandAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import ButtonOne from './ButtonOne';
 
+export default function SpotlightCard(props) {
+	const numberOfImages = props.img.length;
+	const [ imgIndex, setImgIndex ] = useState(0);
+	const [ imgObj, setImgObj ] = useState(props.img[0]);
+	const [ expandedImgDiv, setExpandedImgDiv ] = useState(false);
 
-export default function SpotlightCard(props){
-  const numberOfImages = props.img.length
-  const [imgIndex, setImgIndex] = useState(0) 
-  const [imgObj, setImgObj] = useState(props.img[0]);
-  const [expandedImgDiv, setExpandedImgDiv] = useState(false)
-  
-  const handleImgChangeRight = () => {
-    console.log(`first imgIndex ${imgIndex}`)
-    if(imgIndex < (numberOfImages-1)){
-      setImgIndex(imgIndex + 1);
-      setImgObj(props.img[imgIndex +1]);
-    } else{
-      setImgIndex(0);
-      setImgObj(props.img[0]);
-    }
-  };
+	const handleImgChangeRight = () => {
+		console.log(`first imgIndex ${imgIndex}`);
+		if (imgIndex < numberOfImages - 1) {
+			setImgIndex(imgIndex + 1);
+			setImgObj(props.img[imgIndex + 1]);
+		} else {
+			setImgIndex(0);
+			setImgObj(props.img[0]);
+		}
+	};
 
-  const handleImgChangeLeft = () => {
-    console.log(`first imgIndex ${imgIndex}`)
+	const handleImgChangeLeft = () => {
+		console.log(`first imgIndex ${imgIndex}`);
 
-    if(imgIndex > 0) {
-      setImgIndex(imgIndex - 1);
-      setImgObj(props.img[imgIndex-1]);
+		if (imgIndex > 0) {
+			setImgIndex(imgIndex - 1);
+			setImgObj(props.img[imgIndex - 1]);
+		} else {
+			setImgIndex(numberOfImages - 1);
+			setImgObj(props.img[numberOfImages - 1]);
+		}
+	};
 
-    } else{
-      setImgIndex(numberOfImages-1);
-      setImgObj(props.img[numberOfImages-1]);
-
-    }
-  };
-
-  return(
-    <div className="shadowClass flex flex-col justify-center bg-yellow-300 overflow-hidden m-4 xl:w-1/2">
-      <div className="relative w-full">
-        <img className="object-cover w-full h-60 sm:h-120 lg:h-75 z-0" src={imgObj.src} alt={imgObj.alt}/>
-        <div className="absolute bottom-0 my-auto w-full h-full flex items-center  justify-between">
-          <FontAwesomeIcon onClick={e =>{
-            e.preventDefault()
-            handleImgChangeLeft()
-          }} className="no-select cursor-pointer ml-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110" style={{ color: "#ecc94b" }} size="2x" icon={faChevronLeft} />
-          <FontAwesomeIcon onClick={e =>{
-            e.preventDefault()
-            handleImgChangeRight()
-          }} className="no-select cursor-pointer mr-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110" style={{ color: "#ecc94b" }} size="2x" icon={faChevronRight} />
-          <FontAwesomeIcon onClick={e =>{
-            e.preventDefault()
-            setExpandedImgDiv(()=>!expandedImgDiv)
-          }} className="no-select absolute bottom-0 right-0 cursor-pointer m-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110" style={{ color: "#ecc94b" }} size="2x" icon={faExpandAlt} />
-        </div>
-      </div>
-      <div className="flex flex-grow flex-col mb-2">
-        <h3 className="jost mx-2 mt-4 text-xl font-bold">{props.title}</h3>
-        <p className="jost mx-2 mb-2">{props.text}</p>
-        <ButtonOne route="/products" text="Show Me More"/>
-      </div>
-      <iframe height="315" src={props.videoSrc} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"></iframe>
-      {expandedImgDiv ? 
-      <div className="fixed top-0 left-0 min-h-screen min-w-full z-50 bg-gray-800 bg-opacity-90 flex justify-center items-center">
-        <img className="object-contain w-full h-screen z-0" src={imgObj.src} alt={imgObj.alt}/>
-        <div className="absolute bottom-0 my-auto w-full h-full flex items-center  justify-between">
-          <FontAwesomeIcon onClick={e =>{
-            e.preventDefault()
-            handleImgChangeLeft()
-          }} className="no-select cursor-pointer ml-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110" style={{ color: "#ecc94b" }} size="2x" icon={faChevronLeft} />
-          <FontAwesomeIcon onClick={e =>{
-            e.preventDefault()
-            handleImgChangeRight()
-          }} className="no-select cursor-pointer mr-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110" style={{ color: "#ecc94b" }} size="2x" icon={faChevronRight} />
-          <FontAwesomeIcon onClick={e =>{
-            e.preventDefault()
-            setExpandedImgDiv(()=>!expandedImgDiv)
-          }} className="no-select absolute top-0 right-0 cursor-pointer mr-4 mt-2 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110" style={{ color: "#ecc94b" }} size="2x" icon={faTimes} />
-        </div>
-      </div>: null
-      }
-    </div>
-  )
+	return (
+		<div className="shadowClass flex flex-col justify-center bg-yellow-300 overflow-hidden m-4 xl:w-1/2">
+			<div className="relative w-full">
+				<img className="object-cover w-full h-60 sm:h-120 lg:h-75 z-0" src={imgObj.src} alt={imgObj.alt} />
+				<div className="absolute bottom-0 my-auto w-full h-full flex items-center  justify-between">
+					<FontAwesomeIcon
+						onClick={(e) => {
+							e.preventDefault();
+							handleImgChangeLeft();
+						}}
+						className="no-select cursor-pointer ml-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110"
+						style={{ color: '#ecc94b' }}
+						size="2x"
+						icon={faChevronLeft}
+					/>
+					<FontAwesomeIcon
+						onClick={(e) => {
+							e.preventDefault();
+							handleImgChangeRight();
+						}}
+						className="no-select cursor-pointer mr-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110"
+						style={{ color: '#ecc94b' }}
+						size="2x"
+						icon={faChevronRight}
+					/>
+					<FontAwesomeIcon
+						onClick={(e) => {
+							e.preventDefault();
+							setExpandedImgDiv(() => !expandedImgDiv);
+						}}
+						className="no-select absolute bottom-0 right-0 cursor-pointer m-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110"
+						style={{ color: '#ecc94b' }}
+						size="2x"
+						icon={faExpandAlt}
+					/>
+				</div>
+			</div>
+			<div className="flex flex-grow flex-col mb-2">
+				<h3 className="jost mx-2 mt-4 text-xl font-bold">{props.title}</h3>
+				<p className="jost mx-2 mb-2">{props.text}</p>
+				<ButtonOne route="/products" text="Show Me More" />
+			</div>
+			<iframe
+				height="315"
+				src={props.videoSrc}
+				frameBorder="0"
+				allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+			/>
+			{expandedImgDiv ? (
+				<div className="fixed top-0 left-0 min-h-screen min-w-full z-50 bg-gray-800 bg-opacity-90 flex justify-center items-center">
+					<img className="object-contain w-full h-screen z-0" src={imgObj.src} alt={imgObj.alt} />
+					<div className="absolute bottom-0 my-auto w-full h-full flex items-center  justify-between">
+						<FontAwesomeIcon
+							onClick={(e) => {
+								e.preventDefault();
+								handleImgChangeLeft();
+							}}
+							className="no-select cursor-pointer ml-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110"
+							style={{ color: '#ecc94b' }}
+							size="2x"
+							icon={faChevronLeft}
+						/>
+						<FontAwesomeIcon
+							onClick={(e) => {
+								e.preventDefault();
+								handleImgChangeRight();
+							}}
+							className="no-select cursor-pointer mr-1 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110"
+							style={{ color: '#ecc94b' }}
+							size="2x"
+							icon={faChevronRight}
+						/>
+						<FontAwesomeIcon
+							onClick={(e) => {
+								e.preventDefault();
+								setExpandedImgDiv(() => !expandedImgDiv);
+							}}
+							className="no-select absolute top-0 right-0 cursor-pointer mr-4 mt-2 opacity-75 transition duration-700 ease-in-out transform sm:hover:opacity-100 sm:hover:scale-110"
+							style={{ color: '#ecc94b' }}
+							size="2x"
+							icon={faTimes}
+						/>
+					</div>
+				</div>
+			) : null}
+		</div>
+	);
 }
 
 // lg:object-fill for img?
